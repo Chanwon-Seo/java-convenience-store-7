c0221e351ebf94f23e935fe7a0d204fa208200b0
Chanwon-Seo <scwonn60@gmail.com>
2024-11-08 03:01:40 +0900

    remove: 불필요 전역 상수 클래스 제거

--- src/main/java/store/constants/ParserConstants.java
+++
@@ -1,9 +0,0 @@
-package store.constants;
-
-public class ParserConstants {
-    public static final String PRODUCT_HEADER = "name,price,quantity,promotion";
-    public static final String PROMOTION_HEADER = "name,buy,get,start_date,end_date";
-    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
-    public static final String MIDNIGHT_TIME_FORMAT = "T00:00:00";
-
-}

4247e083cac5ed7b79a1ea6e02bd8e9ca8291502
Chanwon-Seo <scwonn60@gmail.com>
2024-11-07 22:37:25 +0900

    refactor : 명확하지 않은 매개변수 명 수정

--- src/main/java/store/exception/FileReaderException.java
+++ src/main/java/store/exception/FileReaderException.java
@@ -7,14 +7,14 @@ import static store.message.ErrorMessage.INVALID_HEADER;
 import java.util.List;
 
 public class FileReaderException {
-    public static void validateProductHeader(List<String> data) {
-        if (!data.getFirst().equals(PRODUCT_HEADER)) {
+    public static void validateProductHeader(List<String> products) {
+        if (!PRODUCT_HEADER.equals(products.getFirst())) {
             throw new IllegalArgumentException(INVALID_HEADER.getMessage());
         }
     }
 
-    public static void validatePromotionHeader(List<String> data) {
-        if (!data.getFirst().equals(PROMOTION_HEADER)) {
+    public static void validatePromotionHeader(List<String> promotions) {
+        if (!PROMOTION_HEADER.equals(promotions.getFirst())) {
             throw new IllegalArgumentException(INVALID_HEADER.getMessage());
         }
     }

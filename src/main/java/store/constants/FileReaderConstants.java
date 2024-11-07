707318290d87aa6261fe06cf7549354181567838
Chanwon-Seo <scwonn60@gmail.com>
2024-11-08 03:04:35 +0900

    remove: 불필요 전역 상수 클래스 제거

--- src/main/java/store/constants/FileReaderConstants.java
+++
@@ -1,7 +0,0 @@
-package store.constants;
-
-public class FileReaderConstants {
-    public static final String RESOURCES_DIRECTORY = "src/main/resources";
-    public static final String PRODUCTS_FILE = "products.md";
-    public static final String PROMOTIONS_FILE = "promotions.md";
-}

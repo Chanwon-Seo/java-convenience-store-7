c74f893c96d1cfb7e7aa3ce6e97dc989c0783080
Chanwon-Seo <scwonn60@gmail.com>
2024-11-07 22:09:34 +0900

    refactor : ParserService 제거

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,19 +1,24 @@
 package store.controller;
 
+import java.util.List;
+import store.domain.Product;
+import store.domain.Promotion;
 import store.dto.StoreDto;
 import store.dto.StoreInitializationDto;
+import store.parser.ProductParser;
+import store.parser.PromotionParser;
 import store.service.FileReaderService;
-import store.service.ParserService;
 
 public class StoreController {
 
     private final FileReaderService fileReaderService;
-    private final ParserService parserService;
+    private final PromotionParser promotionParser;
+    private final ProductParser productParser;
 
     public StoreController() {
         this.fileReaderService = new FileReaderService();
-        this.parserService = new ParserService();
-
+        this.productParser = new ProductParser();
+        this.promotionParser = new PromotionParser();
     }
 
     public StoreDto initialize() {
@@ -22,7 +27,9 @@ public class StoreController {
     }
 
     public StoreDto parse(StoreInitializationDto storeInitializationDto) {
-        return parserService.parseStoreInitialization(storeInitializationDto);
+        List<Promotion> promotions = promotionParser.parse(storeInitializationDto.promotions());
+        List<Product> products = productParser.parse(storeInitializationDto.products(), promotions);
+        return StoreDto.of(products, promotions);
     }
 
 }

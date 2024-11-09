ff57b84d3a7b098f1801e18bbdb7fb359ffa68de
Chanwon-Seo <scwonn60@gmail.com>
2024-11-10 06:40:00 +0900

    test : 사용자 입력 파싱 검증 테스트

--- src/test/java/store/parser/InputParserTest.java
+++ src/test/java/store/parser/InputParserTest.java
@@ -1,21 +1,74 @@
 package store.parser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import store.dto.OrderItemDto;
 
 class InputParserTest {
     private final InputParser inputParser = new InputParser();
 
     @ParameterizedTest
-    @MethodSource("provideValidInputs")
-    void 유효한_입력_테스트(String input) {
+    @MethodSource("provideOrderItems")
+    void 주문_수량_파싱_테스트(String input, List<OrderItemDto> expectedOrderItems) {
+        List<OrderItemDto> orderItemDtos = inputParser.parseOrderItems(input);
+
+        assertEquals(expectedOrderItems, orderItemDtos);
+    }
+
+    static Stream<Arguments> provideOrderItems() {
+        return Stream.of(
+                Arguments.of(
+                        "[물-1]",
+                        List.of(new OrderItemDto("물", 1))
+                ),
+                Arguments.of(
+                        "[콜라-1],[사이다-1]",
+                        List.of(new OrderItemDto("콜라", 1), new OrderItemDto("사이다", 1))
+                )
+        );
+    }
+
+    @Test
+    void 수량이_잘못된_입력_테스트() {
+        String input = "[물-물]";
+        assertThrows(IllegalArgumentException.class,
+                () -> inputParser.parseOrderItems(input));
+    }
+
+    @Test
+    void 사용자_입력이_Y인_경우_테스트() {
+        String input = "Y";
+
+        boolean b = inputParser.parseYesOrNo(input);
+
+        assertTrue(b);
+    }
+
+    @Test
+    void 사용자_입력이_N인_경우_테스트() {
+        String input = "N";
+
+        boolean b = inputParser.parseYesOrNo(input);
+
+        assertFalse(b);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideValidOrderItemsInputs")
+    void 유효한_주문_입력_테스트(String input) {
         inputParser.validateOrderItems(input);
     }
 
-    static Stream<String> provideValidInputs() {
+    static Stream<String> provideValidOrderItemsInputs() {
         return Stream.of(
                 "[사이다-2],[감자칩-1]",
                 "[사이다-2]",
@@ -24,14 +77,14 @@ class InputParserTest {
     }
 
     @ParameterizedTest
-    @MethodSource("provideInvalidInputs")
-    void 유효하지_않은_입력_테스트(String input) {
+    @MethodSource("provideInvalidOrderItemsInputs")
+    void 유효하지_않은_주문_입력_테스트(String input) {
         assertThrows(IllegalArgumentException.class,
                 () -> inputParser.validateOrderItems(input));
     }
 
 
-    static Stream<String> provideInvalidInputs() {
+    static Stream<String> provideInvalidOrderItemsInputs() {
         return Stream.of(
                 "]1-1[",                   // 닫는 괄호가 먼저 나오는 경우
                 "[1-1",                    // 여는 괄호만 있는 경우
@@ -40,6 +93,26 @@ class InputParserTest {
                 "[[[[[[[[[사이다-2],[감자칩-1]", // 과도한 중첩된 여는 괄호
                 "[[사이다-2]]",             // 중첩된 괄호로 둘러싸인 항목
                 "사이다-2"                   // 괄호가 없는 경우
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidYNInputs1")
+    void 유효하지_않은_예_아니요_입력_테스트(String input) {
+        assertThrows(IllegalArgumentException.class,
+                () -> inputParser.validateYesOrNo(input));
+    }
+
+
+    static Stream<String> provideInvalidYNInputs1() {
+        return Stream.of(
+                "Y ", // 공백이 포함된 경우
+                "N ",
+                " Y",
+                "A", //YN이 아닌 경우
+                "",
+                " ",
+                null
         );
     }
 }

bc2a872e9806874204b5766a2f36dda8bf7636b4
Chanwon-Seo <scwonn60@gmail.com>
2024-11-10 10:53:13 +0900

    test : CartItem 도메인 테스트

---
+++ src/test/java/store/domain/CartItemTest.java
@@ -0,0 +1,51 @@
+package store.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import store.TestDataUtil;
+
+class CartItemTest {
+    private Store store;
+
+    @BeforeEach
+    void setUp() {
+        store = TestDataUtil.createStore();
+    }
+
+    @Test
+    void 카트아이템_생성_테스트() {
+        List<Product> products = store.findProductsByName("콜라");
+        String expectedProductName = "콜라";
+        int expectedQuantity = 10;
+
+        CartItem cartItem = new CartItem(products, 10);
+
+        assertEquals(cartItem.getProductName(), expectedProductName);
+        assertEquals(cartItem.getQuantity(), expectedQuantity);
+    }
+
+    @Test
+    void 카트아이템_추가_테스트() {
+        List<Product> products = store.findProductsByName("콜라");
+        int expectedQuantity = 9;
+
+        CartItem cartItem = new CartItem(products, 8);
+        cartItem.increaseQuantity(1);
+
+        assertEquals(cartItem.getQuantity(), expectedQuantity);
+    }
+
+    @Test
+    void 카트아이템_감소_테스트() {
+        List<Product> products = store.findProductsByName("콜라");
+        int expectedQuantity = 6;
+
+        CartItem cartItem = new CartItem(products, 10);
+        cartItem.decreaseQuantity(4);
+
+        assertEquals(cartItem.getQuantity(), expectedQuantity);
+    }
+}

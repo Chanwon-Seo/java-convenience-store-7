057e5f26f067c9ffd793bd4546174e8fbdae8dc6
Chanwon-Seo <scwonn60@gmail.com>
2024-11-10 13:45:44 +0900

    refactor : DateUtil 파싱할 수 없는 날짜 검증 기능 구현

--- src/main/java/store/util/DateUtil.java
+++ src/main/java/store/util/DateUtil.java
@@ -1,7 +1,10 @@
 package store.util;
 
+import static store.message.ErrorMessage.FILE_READ_ERROR;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class DateUtil {
     private static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
@@ -9,7 +12,12 @@ public class DateUtil {
 
     public static LocalDateTime dateParse(String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
-        return LocalDateTime.parse(date + MIDNIGHT_TIME_FORMAT, formatter);
+
+        try {
+            return LocalDateTime.parse(date + MIDNIGHT_TIME_FORMAT, formatter);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException(FILE_READ_ERROR.getMessage());
+        }
     }
 
 }

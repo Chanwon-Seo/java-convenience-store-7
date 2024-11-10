0edef92a8d1c9a5b6b6b43b6718ca418de51bdad
Chanwon-Seo <scwonn60@gmail.com>
2024-11-10 12:20:33 +0900

    test : 파일 헤더 검증 테스트

--- src/main/java/store/validation/FileReaderValidator.java
+++ src/main/java/store/validation/FileReaderValidator.java
@@ -10,13 +10,13 @@ public abstract class FileReaderValidator {
 
     public static void validateProductHeader(List<String> products) {
         if (!PRODUCT_HEADER.equals(products.getFirst())) {
-            throw new IllegalArgumentException(INVALID_HEADER.getMessage());
+            throw new IllegalStateException(INVALID_HEADER.getMessage());
         }
     }
 
     public static void validatePromotionHeader(List<String> promotions) {
         if (!PROMOTION_HEADER.equals(promotions.getFirst())) {
-            throw new IllegalArgumentException(INVALID_HEADER.getMessage());
+            throw new IllegalStateException(INVALID_HEADER.getMessage());
         }
     }
 }

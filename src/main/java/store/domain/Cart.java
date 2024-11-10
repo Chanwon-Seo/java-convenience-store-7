e07f638ade657b3a6635ed560b41a6778198bde1
Chanwon-Seo <scwonn60@gmail.com>
2024-11-10 22:09:39 +0900

    refactor : Cart의 value를 List<>로 받고 있는 문제 변경

--- src/main/java/store/domain/Cart.java
+++ src/main/java/store/domain/Cart.java
@@ -1,12 +1,11 @@
 package store.domain;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Cart {
-    private final Map<String, List<CartItem>> cartItems;
+    private final Map<String, CartItem> cartItems;
 
     public Cart() {
         this.cartItems = new LinkedHashMap<>();
@@ -14,16 +13,11 @@ public class Cart {
 
     public void addItem(List<CartItem> cartItems) {
         for (CartItem cartItem : cartItems) {
-            this.cartItems.computeIfAbsent(cartItem.getProductName(), k -> new ArrayList<>())
-                    .add(cartItem);
+            this.cartItems.put(cartItem.getProductName(), cartItem);
         }
     }
 
     public List<CartItem> getAllItemsInCart() {
-        List<CartItem> allCartItems = new ArrayList<>();
-        for (List<CartItem> productList : cartItems.values()) {
-            allCartItems.addAll(productList);
-        }
-        return allCartItems;
+        return cartItems.values().stream().toList();
     }
 }

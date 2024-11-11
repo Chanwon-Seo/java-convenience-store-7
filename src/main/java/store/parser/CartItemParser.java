31395b072bfc12b0d21fd9b925178aa851f51372
Chanwon-Seo <scwonn60@gmail.com>
2024-11-11 22:48:40 +0900

    refactor : CartItemParser 주석 추가

--- src/main/java/store/parser/CartItemParser.java
+++ src/main/java/store/parser/CartItemParser.java
@@ -17,6 +17,11 @@ public class CartItemParser {
                 .toList();
     }
 
+    /**
+     * 선택한 상품명으로 상품정보를 가져옵니다. 프로모션이 적용된 상품이 있다면 우선적으로 프로모션 값을 주입합니다.
+     * <p>
+     * 반대로 일반 상품인 경우 일반 상품의 프로모션 정보를 주입받습니다.
+     */
     public CartItem generateCartItemWithPromotion(CartItemDto cartItemDto, Store store) {
         List<Product> products = store.findByProductName(cartItemDto.productName());
         return createCartItemWithPromotion(cartItemDto, products.getFirst());
